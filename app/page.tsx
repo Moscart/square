ecffed6b53970a36ec1040b6dbbd1bac426ff88c
Moscart <danieltheo.73@gmail.com>
2025-06-11 18:29:11 +0700

    feat: create sidenav

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,144 @@
-import Image from "next/image";
+import { Category2, LogoutCurve, ShoppingCart, Truck } from "iconsax-reactjs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative">
+      {/* Header */}
+      <section
+        className="w-[200px] h-screen flex flex-col justify-between border-r border-border fixed bg-white overflow-y-auto
+          [&::-webkit-scrollbar]:w-2
+        [&::-webkit-scrollbar-track]:bg-gray-100
+        [&::-webkit-scrollbar-thumb]:bg-gray-300"
+      >
+        <div className="">
+          <div className="p-6">
+            <div className="flex items-center gap-1">
+              <div className="">
+                <div className="size-[17.85px] bg-primary rounded-full relative mt-1">
+                  <div className="w-[7.68px] h-[15.75px] bg-primary rounded-tr-full absolute bottom-1/2 left-0"></div>
+                </div>
+              </div>
+              <div className="text-primary font-[661] text-[27.98px]">
+                square
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="p-6 pt-0">
+              <div className="font-satoshi font-medium text-[10px] text-muted mb-2">
+                Menu
+              </div>
+              <div className="flex flex-col gap-3">
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Category2 size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Dashboard</span>
+                  </div>
+                  <div className="float-start size-[18px] flex justify-center items-center rounded-full font-semibold text-white text-[10px] bg-linear-to-r from-[#EEA849] to-[#F46B45]">
+                    4
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Category2 size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Dashboard</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Category2 size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Dashboard</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Category2 size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Dashboard</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 pt-0">
+              <div className="font-satoshi font-medium text-[10px] text-muted mb-2">
+                Integration
+              </div>
+              <div className="flex flex-col gap-3">
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <ShoppingCart size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Stock</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 items-center justify-between text-muted py-2">
+                  <div className="flex gap-3 items-center">
+                    <Truck size="14" variant="TwoTone" />
+                    <span className="font-semibold text-[14px]">Supply</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="p-6 border-t border-border">
+          <div className="flex flex-col gap-6">
+            <div className="flex gap-3 items-center">
+              <div className="">
+                <div className="size-9 rounded-full bg-primary overflow-hidden">
+                  s
+                </div>
+              </div>
+              <div className="">
+                <div className="font-semibold text-[14px] line-clamp-1">
+                  Savannah N
+                </div>
+                <div className="font-satoshi text-[10px] text-muted line-clamp-1">
+                  Food Quality Manager
+                </div>
+              </div>
+            </div>
+            <button className="font-semibold text-[12px] text-error bg-error-light rounded-[4px] py-2 flex gap-2 items-center justify-center">
+              <LogoutCurve size="16" variant="Bulk" />
+              <span>Logout</span>
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {/* Main Content */}
+      <section className="bg-red-500 w-full ps-[200px]">
+        <div className="h-[1000px]">asdf</div>
+        {/* <div className="font-bold text-2xl">Customer</div> */}
+      </section>
     </div>
   );
 }
